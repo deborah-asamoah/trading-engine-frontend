f4344cf7bafcb35952da08fae5490771103a1ab1
Gerald Addo-Tetteh <gerald.addo-tetteh@turntabl.io>
2023-05-21 03:07:13 +0000

    add side bar and route links

--- src/app/features/client/components/sidebar/sidebar.component.ts
+++ src/app/features/client/components/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-
+  @Input() username = '';
 }
