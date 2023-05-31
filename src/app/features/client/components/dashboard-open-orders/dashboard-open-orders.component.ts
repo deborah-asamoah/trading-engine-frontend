852f0850b6dfbbbc14c51844a4d4353b20e67b29
Gerald Addo-Tetteh <gerald.addo-tetteh@turntabl.io>
2023-05-31 05:18:38 +0000

    add selector buttons and dropdown to open orders box && create table to show open orders

--- src/app/features/client/components/dashboard-open-orders/dashboard-open-orders.component.ts
+++ src/app/features/client/components/dashboard-open-orders/dashboard-open-orders.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '@stomp/stompjs';
+import { MarketDataService } from '../../services/market-data/market-data.service';
+import { marketDataUrls } from '../../services/market-data/market-data-service-factory';
 
 @Component({
   selector: 'app-dashboard-open-orders',
   templateUrl: './dashboard-open-orders.component.html',
   styleUrls: ['./dashboard-open-orders.component.scss'],
 })
-export class DashboardOpenOrdersComponent {}
+export class DashboardOpenOrdersComponent implements OnInit {
+  products: string[] = [
+    'MSFT',
+    'NFLX',
+    'GOOGL',
+    'AAPL',
+    'TSLA',
+    'IBM',
+    'ORCL',
+    'AMZN',
+  ];
+
+  constructor(private marketDataService: MarketDataService) {}
+
+  ngOnInit(): void {
+    this.marketDataService
+      .watch(marketDataUrls.orderBookTopic)
+      .subscribe((message: Message) => {
+        console.log(message.body);
+      });
+    this.marketDataService.publish({
+      destination: marketDataUrls.initialOrderBookTopic,
+      body: '',
+    });
+  }
+
+  onOrderSideSelected(side: string) {
+    console.log(side);
+  }
+  onProductSelected(event: Event) {
+    console.log((<HTMLSelectElement>event.target).value);
+  }
+}
