4b5300e139855c918d274a3874edaba6d91b2312
Gerald Addo-Tetteh <gerald.addo-tetteh@turntabl.io>
2023-06-06 15:44:07 +0000

    add loading animation for order box and remove client name from sidebar

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   marketBaseUrl: 'ws://localhost:8081',
   ordersBaseUrl: 'http://localhost:8080/orders',
+  portfoliosBaseUrl: 'http://localhost:8083/api/v1/portfolio',
 };
