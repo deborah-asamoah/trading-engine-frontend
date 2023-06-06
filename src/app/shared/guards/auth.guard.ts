775c98b3425224bb709aa2143892f60eeb24843c
Gerald Addo-Tetteh <gerald.addo-tetteh@turntabl.io>
2023-06-06 16:13:58 +0000

    update packages and add chart js

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthClientService } from '../services/auth-client/auth-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     public authClientService: AuthClientService,
